7653da5a82e1f05f491e6591c1b51255108509f7
iamacook <aaron@safe.global>
2023-03-13 17:36:46 +0100

    fix: remove Safe location constant

--- src/utils/safe-apps.ts
+++ src/utils/safe-apps.ts
@@ -1,7 +1,7 @@
-import { CHAIN_SHORT_NAME, SAFE_URL, DEPLOYMENT_URL } from '@/config/constants'
+import { CHAIN_SHORT_NAME, DEPLOYMENT_URL } from '@/config/constants'
 
 export const getGovernanceAppSafeAppUrl = (chainId: number, address: string): string => {
-  const url = new URL(`${SAFE_URL}/apps/open`)
+  const url = new URL(`${location.origin}/apps/open`)
 
   const shortName = CHAIN_SHORT_NAME[chainId]
   url.searchParams.append('safe', `${shortName}:${address}`)
