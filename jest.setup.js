b26d47f443284baa25468d672acca629cd5506df
iamacook <aaron@safe.global>
2023-06-22 13:45:26 +0200

    fix: mocks

--- jest.setup.js
+++ jest.setup.js
@@ -9,7 +9,7 @@ jest.mock('@web3-onboard/keystone/dist/index', () => jest.fn())
 jest.mock('@web3-onboard/ledger', () => jest.fn())
 jest.mock('@web3-onboard/trezor', () => jest.fn())
 jest.mock('@web3-onboard/walletconnect', () => jest.fn())
-jest.mock('@web3-onboard/tallyho', () => jest.fn())
+jest.mock('@web3-onboard/taho', () => jest.fn())
 
 jest.mock('@web3-onboard/injected-wallets/dist/icons/metamask', () => '')
 jest.mock('@web3-onboard/coinbase/dist/icon', () => '')
@@ -17,7 +17,7 @@ jest.mock('@web3-onboard/keystone/dist/icon', () => '')
 jest.mock('@web3-onboard/walletconnect/dist/icon', () => '')
 jest.mock('@web3-onboard/trezor/dist/icon', () => '')
 jest.mock('@web3-onboard/ledger/dist/icon', () => '')
-jest.mock('@web3-onboard/tallyho/dist/icon', () => '')
+jest.mock('@web3-onboard/taho/dist/icon', () => '')
 
 const mockOnboardState = {
   chains: [],
