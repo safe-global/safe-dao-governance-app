cb8a7273c047ecbd121050180a904eec2de757b0
iamacook <aaron@safe.global>
2023-06-29 18:44:47 +0200

    refactor: reference dark mode via `data-theme`

--- src/components/TypographyChip/index.tsx
+++ src/components/TypographyChip/index.tsx
@@ -2,24 +2,11 @@ import { Typography } from '@mui/material'
 import type { TypographyProps } from '@mui/material'
 import type { ReactElement } from 'react'
 
-import { useIsDarkMode } from '@/hooks/useIsDarkMode'
+import css from './styles.module.css'
 
 export const TypographyChip = ({ children, ...props }: TypographyProps): ReactElement => {
-  const isDarkMode = useIsDarkMode()
-
   return (
-    <Typography
-      variant="body2"
-      sx={(theme) => ({
-        py: 0.5,
-        bgcolor: isDarkMode ? theme.palette.primary.main : theme.palette.secondary.main,
-        color: isDarkMode ? theme.palette.primary.contrastText : undefined,
-        borderRadius: `${theme.shape.borderRadius}px`,
-        display: 'flex',
-        alignItems: 'center',
-      })}
-      {...props}
-    >
+    <Typography variant="body2" className={css.chip} py={0.5} {...props}>
       {children}
     </Typography>
   )
