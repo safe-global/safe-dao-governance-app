cb3590b4581de2b3b8ab057977b7edc50e567a61
Aaron Cook <aaron@safe.global>
2023-07-13 09:54:04 +0200

    [EPIC] SEP #5 - Live claiming data test (#42)

--- src/hooks/useAllocations.ts
+++ src/hooks/useAllocations.ts
@@ -3,7 +3,6 @@ import useSWRImmutable from 'swr/immutable'
 import { AIRDROP_TAGS, VESTING_URL } from '@/config/constants'
 import { useAddress } from '@/hooks/useAddress'
 import { useChainId } from '@/hooks/useChainId'
-import { sameAddress } from '@/utils/addresses'
 
 type Tags = (typeof AIRDROP_TAGS)[keyof typeof AIRDROP_TAGS]
 
@@ -18,19 +17,6 @@ export type Allocation = {
   amount: string
   curve: 0 | 1
   proof: string[]
-}
-
-const MOCK_SEP5_ALLOCATION: Allocation = {
-  tag: 'user_v2',
-  account: '0x5f310dc66F4ecDE9a1769f1B7D75224dA592201e',
-  chainId: 5,
-  contract: '0xf7ec19587A59af367924c301C9b25A02A0BA6b73',
-  vestingId: '0x2347bdc1bab409abe9d16809086a3bad5c5462f3fa20754f017486e031f6df73',
-  durationWeeks: 208,
-  startDate: 1662026400,
-  amount: '0x03c3656232739e0000',
-  curve: 0,
-  proof: [],
 }
 
 const fetchAllocation = async (chainId: string, address: string): Promise<Allocation[]> => {
@@ -48,17 +34,7 @@ const fetchAllocation = async (chainId: string, address: string): Promise<Alloca
     }
 
     // Success
-    const allocations: Allocation[] = await response.json()
-
-    // TODO: Remove mock allocation when vesting is deployed
-    const isCurrentChain = MOCK_SEP5_ALLOCATION.chainId.toString() === chainId
-    const isCurrentAddress = sameAddress(MOCK_SEP5_ALLOCATION.account, address)
-
-    if (isCurrentChain && isCurrentAddress) {
-      allocations.push(MOCK_SEP5_ALLOCATION)
-    }
-
-    return allocations
+    return response.json() as Promise<Allocation[]>
   } catch (err) {
     throw Error(`Error fetching vestings: ${err}`)
   }
