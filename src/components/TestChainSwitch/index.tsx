70f3f31c85aabbc66fae6eb96ab4adae59cdde8f
schmanu <manu@safe.global>
2024-02-21 12:54:09 +0100

    feat: test airdrop on sepolia

--- src/components/TestChainSwitch/index.tsx
+++ src/components/TestChainSwitch/index.tsx
@@ -1,7 +1,7 @@
 import { FormControlLabel, Switch } from '@mui/material'
 import type { ReactElement } from 'react'
 
-import { Chains, IS_PRODUCTION, _DEFAULT_CHAIN_ID } from '@/config/constants'
+import { Chains, IS_PRODUCTION } from '@/config/constants'
 import { useChainId, defaultChainIdStore } from '@/hooks/useChainId'
 
 export const TestChainSwitch = (): ReactElement | null => {
@@ -9,7 +9,7 @@ export const TestChainSwitch = (): ReactElement | null => {
 
   const onToggle = () => {
     defaultChainIdStore.setStore((prev) => {
-      return prev === Chains.GOERLI ? Chains.MAINNET : Chains.GOERLI
+      return prev === Chains.SEPOLIA ? Chains.MAINNET : Chains.SEPOLIA
     })
   }
 
@@ -18,6 +18,9 @@ export const TestChainSwitch = (): ReactElement | null => {
   }
 
   return (
-    <FormControlLabel control={<Switch checked={chainId === Chains.GOERLI} onChange={onToggle} />} label="Use Goerli" />
+    <FormControlLabel
+      control={<Switch checked={chainId === Chains.SEPOLIA} onChange={onToggle} />}
+      label="Use Sepolia"
+    />
   )
 }
