e4eceafbed69d7dc3e0e79f10b2f206341ae6dc8
Manuel Gellfart <manuel.gellfart@gmail.com>
2024-03-14 14:21:46 +0100

    Fix: reduce graph flickering (#61)

--- src/hooks/useLockHistory.ts
+++ src/hooks/useLockHistory.ts
@@ -1,20 +1,22 @@
 import { LockHistory } from '@/utils/boost'
 
+const FAKE_LOCKS = [
+  {
+    day: 0,
+    amount: 1000,
+  },
+  {
+    day: 10,
+    amount: 5000,
+  },
+  {
+    day: 20,
+    amount: 4000,
+  },
+]
+
 export const useLockHistory = (): LockHistory[] => {
-  return [
-    {
-      day: 0,
-      amount: 1000,
-    },
-    {
-      day: 10,
-      amount: 5000,
-    },
-    {
-      day: 20,
-      amount: 4000,
-    },
-  ]
+  return FAKE_LOCKS
 }
 
 export const FAKE_NOW = 40
