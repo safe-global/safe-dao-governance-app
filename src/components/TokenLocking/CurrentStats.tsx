70ab22353ade7d8724364a081b902d6e4db52d28
schmanu <manu@safe.global>
2024-03-25 17:24:00 +0100

    fix: redesign token amount component

--- src/components/TokenLocking/CurrentStats.tsx
+++ src/components/TokenLocking/CurrentStats.tsx
@@ -1,10 +1,6 @@
-import { InfoOutlined } from '@mui/icons-material'
-import { Grid, Paper, Typography, Tooltip, Skeleton } from '@mui/material'
+import { Grid } from '@mui/material'
 import { BigNumberish } from 'ethers'
-import { formatUnits } from 'ethers/lib/utils'
-import { Odometer } from '../Odometer'
-
-import css from './styles.module.css'
+import { TokenAmount } from '../TokenAmount'
 
 export const CurrentStats = ({
   loading,
@@ -17,94 +13,12 @@ export const CurrentStats = ({
 }) => {
   return (
     <Grid container direction="row" spacing={3} justifyContent="space-evenly">
-      <Grid item xs={2}>
-        <Paper
-          sx={{
-            p: 3,
-            backgroundColor: ({ palette }) => palette.background.default,
-            color: ({ palette }) => palette.text.primary,
-            position: 'relative',
-            height: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            gap: 1,
-            alignItems: 'center',
-          }}
-        >
-          <Typography variant="h3" fontWeight={700}>
-            #600
-          </Typography>
-          <Typography variant="caption" fontWeight={700}>
-            Ranking
-          </Typography>
-        </Paper>
-      </Grid>
-      <Grid item xs={5}>
-        <Paper
-          sx={{
-            p: 3,
-            backgroundColor: ({ palette }) => palette.background.default,
-            color: ({ palette }) => palette.text.primary,
-            position: 'relative',
-          }}
-        >
-          <Typography fontWeight={700}>
-            Available
-            <Tooltip title={<Typography>tbd</Typography>} arrow placement="top">
-              <InfoOutlined
-                sx={{
-                  height: '16px',
-                  width: '16px',
-                  mb: '-2px',
-                  ml: 1,
-                }}
-              />
-            </Tooltip>
-          </Typography>
-
-          <Grid item display="flex" alignItems="center">
-            <Typography
-              variant="h3"
-              variantMapping={{
-                h3: 'span',
-              }}
-              className={css.amountDisplay}
-            >
-              {loading ? (
-                <Skeleton />
-              ) : (
-                <>
-                  <Odometer value={Number(formatUnits(safeBalance ?? '0', 18))} decimals={2} /> SAFE
-                </>
-              )}
-            </Typography>
-          </Grid>
-        </Paper>
+      <Grid item xs={6}>
+        <TokenAmount amount={safeBalance} label="Available to lock" loading={loading} />
       </Grid>
 
-      <Grid item xs={5}>
-        <Paper
-          sx={{
-            p: 3,
-            backgroundColor: ({ palette }) => palette.primary.main,
-            color: ({ palette }) => palette.background.default,
-            position: 'relative',
-          }}
-        >
-          <Typography fontWeight={700}>Locked</Typography>
-
-          <Grid item display="flex" alignItems="center">
-            <Typography
-              variant="h3"
-              variantMapping={{
-                h3: 'span',
-              }}
-              className={css.amountDisplay}
-            >
-              <Odometer value={Number(formatUnits(currentlyLocked ?? 0, 18))} decimals={2} /> SAFE
-            </Typography>
-          </Grid>
-        </Paper>
+      <Grid item xs={6}>
+        <TokenAmount amount={currentlyLocked} label="Locked" loading={loading} />
       </Grid>
     </Grid>
   )
