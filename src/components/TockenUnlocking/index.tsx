7434feb503a63f2b0eba0ac7ae3048a52f230663
schmanu <manu@safe.global>
2024-03-26 13:35:26 +0100

    refactor: move lock amount calculation into hook

--- src/components/TockenUnlocking/index.tsx
+++ src/components/TockenUnlocking/index.tsx
@@ -2,15 +2,13 @@ import { Box, Button, CircularProgress, Grid, Link, Paper, Stack, Typography } f
 
 import NextLink from 'next/link'
 import { AppRoutes } from '@/config/routes'
-import { BigNumber } from 'ethers'
 import { useSafeAppsSDK } from '@gnosis.pm/safe-apps-react-sdk'
-import { createWithdrawTx, isUnlockEvent, isWithdrawEvent, toRelativeLockHistory } from '@/utils/lock'
+import { createWithdrawTx, toRelativeLockHistory } from '@/utils/lock'
 import { useChainId } from '@/hooks/useChainId'
-import { useSafeUserLockingInfos } from '@/hooks/useSafeTokenBalance'
 import PaperContainer from '../PaperContainer'
 import { UnlockStats } from './UnlockStats'
 import { UnlockTokenWidget } from './UnlockTokenWidget'
-import { UnlockEvent, useLockHistory, WithdrawEvent } from '@/hooks/useLockHistory'
+import { useLockHistory } from '@/hooks/useLockHistory'
 import { ChevronLeft } from '@mui/icons-material'
 
 import css from './styles.module.css'
@@ -20,7 +18,7 @@ import { Odometer } from '../Odometer'
 import SafeToken from '@/public/images/token.svg'
 import { useMemo, useState } from 'react'
 import { CHAIN_START_TIMESTAMPS } from '@/config/constants'
-import { isGreater24HoursDiff } from '@/utils/date'
+import { useSummarizedLockHistory } from '@/hooks/useSummarizedLockHistory'
 
 const TokenUnlocking = () => {
   const { sdk } = useSafeAppsSDK()
@@ -30,55 +28,7 @@ const TokenUnlocking = () => {
 
   const relativeLockHistory = useMemo(() => toRelativeLockHistory(lockHistory, startTime), [lockHistory, startTime])
 
-  const currentlyLocked = useMemo(
-    () =>
-      lockHistory.reduce((prev, event) => {
-        switch (event.eventType) {
-          case 'LOCKED':
-            return prev.add(event.amount)
-
-          case 'UNLOCKED':
-            return prev.sub(event.amount)
-
-          case 'WITHDRAWN':
-            return prev
-        }
-      }, BigNumber.from(0)),
-    [lockHistory],
-  )
-  const unlockedTotal = useMemo(
-    () =>
-      lockHistory.reduce((prev, event) => {
-        switch (event.eventType) {
-          case 'LOCKED':
-            return prev
-          case 'UNLOCKED':
-            return prev.add(event.amount)
-          case 'WITHDRAWN':
-            return prev.sub(event.amount)
-        }
-      }, BigNumber.from(0)),
-    [lockHistory],
-  )
-  const withdrawable = useMemo(() => {
-    const unlocks = lockHistory.filter((event) => isUnlockEvent(event)).map((event) => event as UnlockEvent)
-    const withdrawnIds = lockHistory
-      .filter((event) => isWithdrawEvent(event))
-      .map((event) => event as WithdrawEvent)
-      .map((withdraw) => withdraw.unlockIndex)
-
-    // Unlocks that have not been withdrawn and are older than 24h
-    return unlocks.filter(
-      (unlock) =>
-        !withdrawnIds.includes(unlock.unlockIndex) &&
-        isGreater24HoursDiff(Date.parse(unlock.executionDate), Date.now()),
-    )
-  }, [lockHistory])
-
-  const unlockedReady = useMemo(
-    () => withdrawable.reduce((prev, event) => prev.add(event.amount), BigNumber.from(0)),
-    [withdrawable],
-  )
+  const { totalLocked, totalUnlocked, totalWithdrawable } = useSummarizedLockHistory(lockHistory)
 
   const [isWithdrawing, setIsWithdrawing] = useState(false)
 
@@ -103,10 +53,10 @@ const TokenUnlocking = () => {
 
       <Typography variant="h1">Unlock / Withdraw</Typography>
       <PaperContainer sx={{ width: '888px' }}>
-        <UnlockStats currentlyLocked={currentlyLocked} unlockedTotal={unlockedTotal} />
+        <UnlockStats currentlyLocked={totalLocked} unlockedTotal={totalUnlocked} />
       </PaperContainer>
       <PaperContainer sx={{ width: '888px' }}>
-        <UnlockTokenWidget currentlyLocked={currentlyLocked} lockHistory={relativeLockHistory} />
+        <UnlockTokenWidget currentlyLocked={totalLocked} lockHistory={relativeLockHistory} />
       </PaperContainer>
       <PaperContainer sx={{ width: '888px' }}>
         <Typography variant="h4" fontWeight={700}>
@@ -136,7 +86,7 @@ const TokenUnlocking = () => {
                     }}
                     className={css.amountDisplay}
                   >
-                    <Odometer value={Number(formatUnits(unlockedReady ?? '0', 18))} decimals={2} /> SAFE
+                    <Odometer value={Number(formatUnits(totalWithdrawable ?? '0', 18))} decimals={2} /> SAFE
                   </Typography>
                 </Grid>
               </Box>
@@ -144,7 +94,7 @@ const TokenUnlocking = () => {
                 variant="contained"
                 color="primary"
                 onClick={onWithdraw}
-                disabled={unlockedReady.eq(0) || isWithdrawing}
+                disabled={totalWithdrawable.eq(0) || isWithdrawing}
                 sx={{ ml: 'auto !important' }}
               >
                 {isWithdrawing ? <CircularProgress size={20} /> : 'Withdraw'}
