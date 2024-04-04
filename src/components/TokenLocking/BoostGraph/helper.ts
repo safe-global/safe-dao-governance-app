7a2ac710b5032410aef02d4d86c8da2c7c7a369d
Manuel Gellfart <manu@safe.global>
2024-04-04 14:27:36 +0200

    Fix: multiple locks today (#99)

--- src/components/TokenLocking/BoostGraph/helper.ts
+++ src/components/TokenLocking/BoostGraph/helper.ts
@@ -3,8 +3,11 @@ import { LockHistory } from '@/utils/lock'
 
 export const generatePointsFromHistory = (pastLocks: LockHistory[], nowInDays: number): { x: number; y: number }[] => {
   const boostFunction = getBoostFunction(nowInDays, 0, pastLocks)
-  return pastLocks.map((lock) => ({
-    x: lock.day,
-    y: boostFunction({ x: lock.day }),
+  // find each individual day
+  const days = pastLocks.map((lock) => lock.day).filter((day, index, days) => days.indexOf(day) === index)
+
+  return days.map((day) => ({
+    x: day,
+    y: boostFunction({ x: day }),
   }))
 }
