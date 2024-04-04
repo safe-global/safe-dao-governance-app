dab906b5615a123231bc0375c03949416e41506c
schmanu <manu@safe.global>
2024-04-04 15:18:14 +0200

    feat: show next unlock on withdraw widget

--- src/hooks/useSummarizedLockHistory.ts
+++ src/hooks/useSummarizedLockHistory.ts
@@ -6,7 +6,12 @@ import { UnlockEvent, useLockHistory, WithdrawEvent } from './useLockHistory'
 
 export const useSummarizedLockHistory = (
   lockHistory: ReturnType<typeof useLockHistory>,
-): { totalLocked: BigNumber; totalUnlocked: BigNumber; totalWithdrawable: BigNumber } => {
+): {
+  totalLocked: BigNumber
+  totalUnlocked: BigNumber
+  totalWithdrawable: BigNumber
+  nextUnlock: UnlockEvent | undefined
+} => {
   const totalLocked = useMemo(
     () =>
       lockHistory.reduce((prev, event) => {
@@ -37,21 +42,33 @@ export const useSummarizedLockHistory = (
       }, BigNumber.from(0)),
     [lockHistory],
   )
-  const totalWithdrawable = useMemo(() => {
+  const { totalWithdrawable, nextUnlock } = useMemo(() => {
     const unlocks = lockHistory.filter((event) => isUnlockEvent(event)).map((event) => event as UnlockEvent)
     const withdrawnIds = lockHistory
       .filter((event) => isWithdrawEvent(event))
       .map((event) => event as WithdrawEvent)
       .map((withdraw) => withdraw.unlockIndex)
-
     // Unlocks that have not been withdrawn and are older than 24h
     const withdrawableUnlocks = unlocks.filter(
       (unlock) =>
         !withdrawnIds.includes(unlock.unlockIndex) &&
         isGreater24HoursDiff(Date.parse(unlock.executionDate), Date.now()),
     )
-    return withdrawableUnlocks.reduce((prev, event) => prev.add(event.amount), BigNumber.from(0))
+    const totalWithdrawable = withdrawableUnlocks.reduce((prev, event) => prev.add(event.amount), BigNumber.from(0))
+    const nextUnlock = unlocks
+      .filter((unlock) => !isGreater24HoursDiff(Date.parse(unlock.executionDate), Date.now()))
+      .reduce((prev: undefined | UnlockEvent, current) => {
+        // We look for the oldest unlock event that is not older than 24h
+        const currentExecDate = Date.parse(current.executionDate)
+        const prevExecDate = prev ? Date.parse(prev.executionDate) : undefined
+        return prevExecDate === undefined || currentExecDate < prevExecDate ? current : prev
+      }, undefined)
+
+    return {
+      totalWithdrawable,
+      nextUnlock,
+    }
   }, [lockHistory])
 
-  return { totalLocked, totalUnlocked, totalWithdrawable }
+  return { totalLocked, totalUnlocked, totalWithdrawable, nextUnlock }
 }
