0b8ad431e6a6ccf43be06b65f203488da530da0d
schmanu <manu@safe.global>
2024-04-08 11:42:20 +0200

    fix: mobile style of sidebar accordion

--- src/components/TokenLocking/ActivityRewardsInfo.tsx
+++ src/components/TokenLocking/ActivityRewardsInfo.tsx
@@ -1,4 +1,4 @@
-import { Divider, Link, SvgIcon, Typography } from '@mui/material'
+import { Box, Divider, Link, SvgIcon, Typography } from '@mui/material'
 import css from './styles.module.css'
 import clsx from 'clsx'
 import { useChainId } from '@/hooks/useChainId'
@@ -49,98 +49,100 @@ export const ActivityRewardsInfo = () => {
 
   return (
     <AccordionContainer title="How the program works">
-      <PaperContainer sx={{ position: 'relative', overflow: 'hidden' }}>
-        <SvgIcon
-          component={Asterix}
-          inheritViewBox
-          sx={{ color: 'transparent', position: 'absolute', top: 0, right: 0, height: '208px', width: 'inherit' }}
-        />
-
-        <div className={css.steps}>
-          {ownRank && (
-            <>
-              <Typography variant="overline" fontWeight="bold" color="text.secondary" mb="6px">
-                Your current ranking
-              </Typography>
-              <Typography variant="h3" fontWeight="bold" display="flex" alignItems="center" gap={1}>
-                <SvgIcon component={StarIcon} inheritViewBox />#{ownRank.position}
-              </Typography>
-            </>
-          )}
-          <Typography variant="h1" fontWeight={700} mt="52px" mb="40px" maxWidth="60%" className={css.gradientText}>
-            Interact with Safe and get rewards
-          </Typography>
-
-          <Step
-            active={stepsActive[0]}
-            title="Lock SAFE to boost your miles!"
-            description="Lock your SAFE tokens early and increase your mile earning power. The earlier and more you lock, the bigger
-          your miles multiplier."
+      <Box display="flex" flexDirection="column">
+        <PaperContainer sx={{ position: 'relative', overflow: 'hidden' }}>
+          <SvgIcon
+            component={Asterix}
+            inheritViewBox
+            sx={{ color: 'transparent', position: 'absolute', top: 0, right: 0, height: '208px', width: 'inherit' }}
           />
 
-          <Step
-            active={stepsActive[1]}
-            activationDate="June 10"
-            title={
+          <div className={css.steps}>
+            {ownRank && (
               <>
-                Get activity miles
-                {!stepsActive[1] && (
-                  <Typography variant="caption" component="span" className={css.comingSoon}>
-                    Coming soon
-                  </Typography>
-                )}
+                <Typography variant="overline" fontWeight="bold" color="text.secondary" mb="6px">
+                  Your current ranking
+                </Typography>
+                <Typography variant="h3" fontWeight="bold" display="flex" alignItems="center" gap={1}>
+                  <SvgIcon component={StarIcon} inheritViewBox />#{ownRank.position}
+                </Typography>
               </>
-            }
-            description="Earn miles by using your Safe Account. Your miles are multiplied by the boost you build until September 30."
-          />
+            )}
+            <Typography variant="h1" fontWeight={700} mt="52px" mb="40px" maxWidth="60%" className={css.gradientText}>
+              Interact with Safe and get rewards
+            </Typography>
 
-          <Step
-            active={stepsActive[2]}
-            activationDate="September 30"
-            title="Get rewards from your activity miles!"
-            description="Get rewards from your boosted activity miles! A higher ranking provides higher chances for rewards."
-          />
-        </div>
+            <Step
+              active={stepsActive[0]}
+              title="Lock SAFE to boost your miles!"
+              description="Lock your SAFE tokens early and increase your mile earning power. The earlier and more you lock, the bigger
+          your miles multiplier."
+            />
 
-        <div className={css.rewards}>
-          <div className={css.rewardsBackground} />
+            <Step
+              active={stepsActive[1]}
+              activationDate="June 10"
+              title={
+                <>
+                  Get activity miles
+                  {!stepsActive[1] && (
+                    <Typography variant="caption" component="span" className={css.comingSoon}>
+                      Coming soon
+                    </Typography>
+                  )}
+                </>
+              }
+              description="Earn miles by using your Safe Account. Your miles are multiplied by the boost you build until September 30."
+            />
 
-          <div className={css.diamondImage}>
-            <Image src={Diamond} alt="Diamond image" width={200} height={200} />
+            <Step
+              active={stepsActive[2]}
+              activationDate="September 30"
+              title="Get rewards from your activity miles!"
+              description="Get rewards from your boosted activity miles! A higher ranking provides higher chances for rewards."
+            />
           </div>
 
-          <Typography variant="h1" fontWeight={700} textAlign="center" mt={-3} mb={1} className={css.gradientText}>
-            SAFE Tokens
-          </Typography>
-          <Typography textAlign="center">2.5M SAFE to be distributed</Typography>
+          <div className={css.rewards}>
+            <div className={css.rewardsBackground} />
 
-          <Typography textAlign="center" color="text.secondary" mt={10} mb={1}>
-            More rewards
-          </Typography>
-          <Typography
-            sx={{
-              border: ({ palette }) => `1px solid ${palette.primary.main}`,
-              borderRadius: '6px',
-              width: 'fit-content',
-              margin: 'auto',
-              padding: 1,
-              textTransform: 'uppercase',
-              textAlign: 'center',
-            }}
-          >
-            Coming soon
-          </Typography>
-        </div>
+            <div className={css.diamondImage}>
+              <Image src={Diamond} alt="Diamond image" width={200} height={200} />
+            </div>
 
-        <Divider />
+            <Typography variant="h1" fontWeight={700} textAlign="center" mt={-3} mb={1} className={css.gradientText}>
+              SAFE Tokens
+            </Typography>
+            <Typography textAlign="center">2.5M SAFE to be distributed</Typography>
 
-        <Link href="#" sx={{ textAlign: 'center', fontSize: '14px' }}>
-          More info about Activity Miles
+            <Typography textAlign="center" color="text.secondary" mt={10} mb={1}>
+              More rewards
+            </Typography>
+            <Typography
+              sx={{
+                border: ({ palette }) => `1px solid ${palette.primary.main}`,
+                borderRadius: '6px',
+                width: 'fit-content',
+                margin: 'auto',
+                padding: 1,
+                textTransform: 'uppercase',
+                textAlign: 'center',
+              }}
+            >
+              Coming soon
+            </Typography>
+          </div>
+
+          <Divider />
+
+          <Link href="#" sx={{ textAlign: 'center', fontSize: '14px' }}>
+            More info about Activity Miles
+          </Link>
+        </PaperContainer>
+        <Link href={AppRoutes.terms} component={NextLink} m={2} sx={{ textAlign: 'center' }}>
+          Terms and Conditions
         </Link>
-      </PaperContainer>
-      <Link href={AppRoutes.terms} component={NextLink} sx={{ textAlign: 'center' }}>
-        Terms and Conditions
-      </Link>
+      </Box>
     </AccordionContainer>
   )
 }
