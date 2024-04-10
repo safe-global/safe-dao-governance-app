1e29524af4263f271807b01b4a3e9cf7e625eb72
James Mealy <james@safe.global>
2024-04-10 16:14:28 +0200

    change lock/unlock receipt text and layout

--- src/components/TokenLocking/MilesReceipt.tsx
+++ src/components/TokenLocking/MilesReceipt.tsx
@@ -1,8 +1,8 @@
 import { Button, IconButton, Modal, Stack, SvgIcon, Typography } from '@mui/material'
 import css from './styles.module.css'
 import SafeExplorers from '@/public/images/safe-explorers.svg'
-import Barcode from '@/public/images/barcode.svg'
 import { NorthRounded, SouthRounded } from '@mui/icons-material'
+import XIcon from '@mui/icons-material/X'
 import CloseIcon from '@mui/icons-material/Close'
 import clsx from 'clsx'
 import { formatAmount } from '@/utils/formatters'
@@ -25,68 +25,75 @@ const MilesReceipt = ({
 
   return (
     <Modal open={open} onClose={onClose} slotProps={{ backdrop: { sx: { backgroundColor: '#636669BF' } } }}>
-      <div className={css.milesReceipt}>
+      <>
         <div className={clsx(css.gradientBackground, { [css.unlockGradient]: isUnlock })} />
-        <Stack className={css.leftReceipt}>
+        <Stack className={css.milesReceipt}>
           <SvgIcon component={SafeExplorers} inheritViewBox sx={{ width: '282px', height: 'auto' }} />
-          <Stack mt="auto">
-            <Typography variant="h1" component="div" fontWeight="bold" mb={1}>
-              {isUnlock ? 'Your tokens are unlocked' : 'Welcome on board'}
+          <Stack mt="auto" className={css.topReceipt}>
+            <Typography variant="h1" component="div" fontWeight="bold" mb={1} mt={7}>
+              {isUnlock ? 'Your tokens are unlocked' : 'Locking started...'}
             </Typography>
-            <Typography mb={5}>
-              You successfully started {isUnlock ? 'unlocking' : 'locking'} {amount} SAFE. Once the transaction is
-              signed and executed the changes will be reflected in this App.
-              <br />
-              {isUnlock ? 'Withdrawal will be available in 24h' : ''}
+            <Typography mb={4}>
+              You {isUnlock ? 'unlocked' : 'successfully started locking'} {amount} SAFE.
+              {isUnlock ? (
+                ' Withdrawal will be available in 24h'
+              ) : (
+                <>
+                  <br />
+                  Once the transaction is signed and executed the changes will be reflected in this App.
+                </>
+              )}
             </Typography>
 
             {!isUnlock && (
-              <Stack direction="row" gap={2}>
+              <Stack direction="row" gap={2} mb={2}>
                 {/* TODO: Need content to share message on Twitter */}
                 <Button variant="outlined" href="https://twitter.com/safe" target="_blank">
-                  Share on X
+                  Share on <XIcon sx={{ ml: 1, fontSize: '20px' }} />
                 </Button>
                 <Button variant="outlined" href="https://twitter.com/safe" target="_blank">
-                  Follow Safe on X
+                  Follow Safe on <XIcon sx={{ ml: 1, fontSize: '20px' }} />
                 </Button>
               </Stack>
             )}
           </Stack>
-        </Stack>
-
-        <Stack className={css.rightReceipt} gap={3} justifyContent="center">
-          <Typography variant="h4" fontWeight="bold">
-            Your activity update
-          </Typography>
-
-          <Stack direction="row" alignItems="baseline">
-            <ArrowIcon color={isUnlock ? 'warning' : 'primary'} fontSize="small" />
-            <Typography variant="h2" component="div" fontWeight="bold" color={isUnlock ? 'warning.main' : 'primary'}>
-              {formatAmount(amount, 0)}
+          <Stack gap={3} justifyContent="center" mt={6}>
+            <Typography variant="h4" fontWeight="bold">
+              Your activity update
             </Typography>
-            <Typography color="text.secondary" ml={1}>
-              Tokens {isUnlock ? 'unlocked' : 'locked'}
-            </Typography>
-          </Stack>
 
-          <div>
             <Stack direction="row" alignItems="baseline">
               <ArrowIcon color={isUnlock ? 'warning' : 'primary'} fontSize="small" />
               <Typography variant="h2" component="div" fontWeight="bold" color={isUnlock ? 'warning.main' : 'primary'}>
-                {formatAmount(floorNumber(newFinalBoost, 2), 2)}x
+                {formatAmount(amount, 0)}
               </Typography>
               <Typography color="text.secondary" ml={1}>
-                Your new miles boost
+                Tokens {isUnlock ? 'unlocked' : 'locked'}
               </Typography>
             </Stack>
-            <Typography>This boost will be applied to all your miles earned</Typography>
-          </div>
-          {!isUnlock && <Barcode className={css.barcode} />}
-          <IconButton className={css.closeButton} onClick={onClose}>
-            <CloseIcon fontSize="large" />
-          </IconButton>
+
+            <div>
+              <Stack direction="row" alignItems="baseline">
+                <ArrowIcon color={isUnlock ? 'warning' : 'primary'} fontSize="small" />
+                <Typography
+                  variant="h2"
+                  component="div"
+                  fontWeight="bold"
+                  color={isUnlock ? 'warning.main' : 'primary'}
+                >
+                  {formatAmount(floorNumber(newFinalBoost, 2), 2)}x
+                </Typography>
+                <Typography color="text.secondary" ml={1}>
+                  Your new miles boost
+                </Typography>
+              </Stack>
+            </div>
+            <IconButton className={css.closeButton} onClick={onClose}>
+              <CloseIcon fontSize="large" />
+            </IconButton>
+          </Stack>
         </Stack>
-      </div>
+      </>
     </Modal>
   )
 }
