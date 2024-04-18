c2e59f08cf26c22d85a82602825e92528c9ee5ce
schmanu <manu@safe.global>
2024-04-18 17:06:30 +0200

    fix: delete unused page

--- pages/boost.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from 'next'
-
-import WhatIsBoost from '@/components/WhatIsBoost'
-
-const BoostPage: NextPage = () => {
-  return <WhatIsBoost />
-}
-
-export default BoostPage
