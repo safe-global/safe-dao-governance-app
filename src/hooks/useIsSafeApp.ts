606073368165761cede904ceb0ad9bbe9799211c
schmanu <manu@safe.global>
2024-04-19 11:06:24 +0200

    fix: detection if opened as Safe App

--- src/hooks/useIsSafeApp.ts
+++ src/hooks/useIsSafeApp.ts
@@ -1,6 +1,15 @@
 import { useSafeAppsSDK } from '@safe-global/safe-apps-react-sdk'
+import { useEffect, useState } from 'react'
 
 export const useIsSafeApp = (): boolean => {
   const { connected } = useSafeAppsSDK()
-  return connected
+  const [isSafeApp, setIsSafeApp] = useState(connected)
+
+  useEffect(() => {
+    const isApp = connected || (typeof window !== 'undefined' && window.self !== window.top)
+    if (isApp !== isSafeApp) {
+      setIsSafeApp(isApp)
+    }
+  }, [connected])
+  return isSafeApp
 }
