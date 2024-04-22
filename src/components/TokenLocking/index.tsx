48fc7401841e4048f0dbd79fafa3f65dd890fa84
James Mealy <james@safe.global>
2024-04-22 17:02:15 +0200

    fix: padding for legal disclaimer on mobile

--- src/components/TokenLocking/index.tsx
+++ src/components/TokenLocking/index.tsx
@@ -50,7 +50,7 @@ const TokenLocking = () => {
           <PaperContainer>
             <Leaderboard />
           </PaperContainer>
-          <Box>
+          <Box px={{ xs: 4, md: 0 }}>
             <Typography variant="overline" fontWeight="bold" color="text.secondary">
               LEGAL DISCLAIMER
             </Typography>
@@ -71,7 +71,7 @@ const TokenLocking = () => {
               DOES NOT GRANT YOU ANY CONTRACTUAL CLAIM TO RECEIVE REWARDS.
             </Typography>
           </Box>
-          <ExternalLink href={SAFE_TERMS_AND_CONDITIONS_URL} m={2}>
+          <ExternalLink href={SAFE_TERMS_AND_CONDITIONS_URL} m={2} px={{ xs: 4, md: 0 }}>
             Terms and Conditions
           </ExternalLink>
         </Stack>
