def4c1941f86257b9ad438f1539bc6f08e6b2181
Manuel Gellfart <manu@safe.global>
2024-06-24 15:46:16 +0200

    feat: show weekly update separately, redesign some components (#186)

--- src/components/Points/CampaignTabs.tsx
+++ src/components/Points/CampaignTabs.tsx
@@ -1,18 +1,44 @@
-import * as React from 'react'
 import Tabs from '@mui/material/Tabs'
 import Tab from '@mui/material/Tab'
-import { Box, Chip, Typography, useMediaQuery } from '@mui/material'
+import { Box, Chip, Tooltip, Typography } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
+import css from './styles.module.css'
 
 const CAMPAIGN_TABS = [
   {
-    label: 'Global',
+    label: (
+      <Typography display="flex" flexDirection="row" gap={1} alignItems="center" fontWeight={700}>
+        <Tooltip title="This campaign is active now" arrow>
+          <Box
+            sx={{
+              borderRadius: '100%',
+              backgroundColor: ({ palette }) => palette.primary.main,
+              minWidth: '6px',
+              minHeight: '6px',
+              flexShrink: 0,
+              marginRight: 1,
+            }}
+          />
+        </Tooltip>
+        Global
+      </Typography>
+    ),
     disabled: false,
   },
   {
     label: (
-      <Typography display="flex" flexDirection="row" gap={1} alignItems="center">
-        Campaigns <Chip variant="outlined" sx={{ borderRadius: '4px' }} label="soon" />
+      <Typography display="flex" flexDirection="row" gap={1} alignItems="center" fontWeight={700}>
+        <Box
+          sx={{
+            borderRadius: '100%',
+            backgroundColor: ({ palette }) => palette.text.disabled,
+            minWidth: '6px',
+            minHeight: '6px',
+            flexShrink: 0,
+            marginRight: 1,
+          }}
+        />
+        Campaigns <Chip variant="outlined" className={css.comingSoon} label="soon" />
       </Typography>
     ),
     disabled: true,
@@ -21,19 +47,22 @@ const CAMPAIGN_TABS = [
 
 const CampaignTabs = ({ onChange, selectedTabIdx }: { onChange: (tab: number) => void; selectedTabIdx: number }) => {
   const theme = useTheme()
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down('lg'))
   return (
     <Box padding="8px 0px">
       <Tabs
-        orientation={isSmallScreen ? 'horizontal' : 'vertical'}
+        orientation="vertical"
         variant="scrollable"
         value={selectedTabIdx}
         aria-label="Vertical tabs example"
-        sx={{ borderRight: 1, borderColor: 'divider' }}
+        sx={{ border: 1, borderColor: 'divider', borderRadius: '6px' }}
         onChange={(_, value) => onChange(value)}
       >
         {CAMPAIGN_TABS.map((tab, tabIdx) => (
-          <Tab key={tabIdx} {...tab} />
+          <Tab
+            sx={{ textTransform: 'none', fontWeight: 700, textAlign: 'left', alignItems: 'start' }}
+            key={tabIdx}
+            {...tab}
+          />
         ))}
       </Tabs>
     </Box>
--- src/components/Points/index.tsx
+++ src/components/Points/index.tsx
@@ -1,8 +1,8 @@
 import { GLOBAL_CAMPAIGN_IDS } from '@/config/constants'
 import { useCampaignInfo, useCampaignPage } from '@/hooks/useCampaigns'
 import { useChainId } from '@/hooks/useChainId'
-import { Grid, Typography, Stack, Box } from '@mui/material'
-import { useEffect, useState } from 'react'
+import { Grid, Typography, Stack, Box, SvgIcon, Divider, useMediaQuery } from '@mui/material'
+import { useEffect, useMemo, useState } from 'react'
 import { ExternalLink } from '../ExternalLink'
 import PaperContainer from '../PaperContainer'
 import SafePassDisclaimer from '../SafePassDisclaimer'
@@ -11,7 +11,10 @@ import { CampaignInfo } from './CampaignInfo'
 import { CampaignLeaderboard } from './CampaignLeaderboard'
 import { CampaignSelector } from './CampaignSelector'
 import CampaignTabs from './CampaignTabs'
+import StarIcon from '@/public/images/leaderboard-title-star.svg'
 import css from './styles.module.css'
+import { TotalPoints } from './TotalPoints'
+import { useTheme } from '@mui/material/styles'
 
 const Points = () => {
   const campaignPage = useCampaignPage(20)
@@ -41,6 +44,13 @@ const Points = () => {
     }
   }
 
+  const isGlobalCampaign = useMemo(
+    () => globalCampaign?.resourceId === selectedCampaignId,
+    [globalCampaign?.resourceId, selectedCampaignId],
+  )
+  const theme = useTheme()
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('lg'))
+
   return (
     <Grid container spacing={3} direction="row">
       <Grid item xs={12} mt={4} mb={1} className={css.pageTitle} display="flex" flexDirection="row" alignItems="center">
@@ -52,8 +62,26 @@ const Points = () => {
 
       <Grid item xs={12} lg={8}>
         <Stack spacing={3}>
+          {isSmallScreen && <TotalPoints />}
           <PaperContainer>
-            <Stack direction={{ lg: 'row', md: 'column' }} spacing={3}>
+            <Stack direction="row" spacing={2}>
+              {!isSmallScreen && <SvgIcon component={StarIcon} inheritViewBox sx={{ width: '27px', height: '27px' }} />}
+              <Stack spacing={1}>
+                <Typography variant="h5" fontWeight={700} fontSize="24px">
+                  Points activity feed
+                </Typography>
+
+                {!isSmallScreen && (
+                  <Typography variant="body2" color="text.secondary">
+                    Earn points by engaging with Safe and our campaign partners. Depending on the campaign, you&apos;ll
+                    be rewarded for various activities suggested by our partners. You can also earn points for regular
+                    Safe activities.
+                  </Typography>
+                )}
+              </Stack>
+            </Stack>
+            <Divider />
+            <Stack direction={{ lg: 'row', md: 'column' }} spacing={4}>
               <CampaignTabs onChange={onTabChange} selectedTabIdx={selectedTab} />
               <Box width="100%">
                 <Stack
@@ -65,10 +93,9 @@ const Points = () => {
                   minHeight="60px"
                 >
                   <Box width="100%">
-                    <Typography variant="h5" fontWeight={700}>
-                      Points activity feed
+                    <Typography variant="h6" fontWeight={700} fontSize="20px">
+                      {isGlobalCampaign ? 'Global' : campaign?.name}
                     </Typography>
-                    <Typography variant="subtitle2">Your points are updated weekly.</Typography>
                   </Box>
                   {selectedTab > 0 && (
                     <CampaignSelector
@@ -89,6 +116,7 @@ const Points = () => {
       </Grid>
       <Grid item xs={12} lg={4}>
         <Stack spacing={3} justifyContent="stretch" height="100%">
+          {!isSmallScreen && <TotalPoints />}
           <CampaignInfo campaign={campaign} />
         </Stack>
       </Grid>
